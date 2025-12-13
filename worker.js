c6f90b9d28657eac778963c7a942ed32363ec7cf
nihal9092 <jdje9jei38@gmail.com>
2025-12-13 19:56:26 +0530

    Delete worker.js

--- worker.js
+++
@@ -1,9 +0,0 @@
-// worker.js (Example snippet for structure)
-export default {
-  async fetch(request, env) {
-    // ... (CORS logic here) ...
-    const BOT_TOKEN = env.TELEGRAM_BOT_TOKEN; 
-    const CHAT_ID = env.TELEGRAM_CHAT_ID;
-    // ... (rest of the logic) ...
-  }
-}
